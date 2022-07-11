4692b4545d9cf6c6dd5c0c8ecaea25d77c52b9b5
s.feli <s.feli@DESKTOP-LOCAL>
2022-07-11 20:07:04 +0430

    add a simple validation to search form

--- src/components/Home/SearchForm.tsx
+++ src/components/Home/SearchForm.tsx
@@ -1,11 +1,43 @@
+import React, { useRef, useState } from 'react';
 import classes from '../../styles/SearchForm.module.scss';
 
-const SearchForm = () => <form className={classes.form}>
-    <h2>Search User</h2>
-    <div className={classes.searchGroup}>
-        <input type="text" className={classes.input} />
-        <button type='submit' className={classes.button}> Search </button>
-    </div>
-</form>
+const SearchForm = () => {
+    const [validationMessage,setValidationMessage] = useState<string>("");
+
+    const userNameRef = useRef<HTMLInputElement>(null);
+
+    const submitHandler = (e: React.FormEvent) => {
+        e.preventDefault();
+        const userName = userNameRef.current!.value;
+        if (userName.trim().length < 3){
+            setValidationMessage("Please enter at least 3 character");
+        }else{
+            debugger;
+        }
+    }
+    
+    const changeInputHandler = (e: React.FormEvent<HTMLInputElement>) => {
+        const enteredValue = e.currentTarget.value;
+        if (enteredValue.trim().length >= 3){
+            setValidationMessage("");
+        }
+    }
+
+    return (
+        <form className={classes.form} onSubmit={submitHandler}>
+            <h2>Search User</h2>
+            <div className={classes.searchGroup}>
+                <input 
+                    type="text" 
+                    className={classes.input} 
+                    ref={userNameRef}
+                    onChange={changeInputHandler}
+                    />
+                <button type='submit' className={classes.button}> Search </button>
+            </div>
+            {validationMessage && <p className={classes.validationMessage}>{validationMessage}</p>}
+        </form>
+    )
+}
 
 export default SearchForm;
