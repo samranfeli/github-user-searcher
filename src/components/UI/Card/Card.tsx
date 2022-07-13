43f198980cf05a6b9f829a4f0f5c02c87366564a
s.feli <s.feli@DESKTOP-LOCAL>
2022-07-13 10:54:20 +0430

    create card component

---
+++ src/components/UI/Card/Card.tsx
@@ -0,0 +1,16 @@
+import classes from '../../../styles/card.module.scss';
+
+type Props = {
+    children: React.ReactNode;
+    cover?:string;
+    className?:string;
+}
+
+const Card:React.FC<Props> = props => <div className={`${classes.card} ${props.className?props.className:""}`}>
+    {props.cover && <div style={{backgroundImage:`url("${props.cover}")`}} className={classes.cover} /> }
+    <div className={classes.cardPadding}>
+        {props.children}
+    </div>
+</div>
+
+export default Card;
