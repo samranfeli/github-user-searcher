3bf76f19d15d05af1c1bd58d7741b56198f8ea6a
s.feli <s.feli@DESKTOP-LOCAL>
2022-07-13 10:58:54 +0430

    create a custom hook for http calls

---
+++ src/hooks/use-http.ts
@@ -0,0 +1,46 @@
+import { useState, useCallback } from "react";
+
+import axios from 'axios';
+
+type RequestConfig = {
+    url:string;
+    method?:'post'| 'get';
+    data ?: any
+}
+type ApplyData = (data:any) => void;
+
+type HookReturn = {
+    loading:boolean; 
+    errorMessage?:string; 
+    sendRequest:(requestConfig:RequestConfig,applyData:any)=>void 
+}
+
+const useHttp : () => HookReturn = () => {
+
+    const [loading,setLoading] = useState(false);
+    const [errorMessage,setErrorMessage] = useState();
+  
+    const sendRequest = useCallback (async(requestConfig:RequestConfig,applyData:ApplyData) => {
+        setLoading(true);
+      try{
+        const response = await axios({
+          method: requestConfig.method || "get",
+          url: requestConfig.url,
+          data: requestConfig.data ?requestConfig.data : null 
+        });
+        setLoading(false);
+        applyData(response.data);
+      }catch (err:any){
+        setLoading(false);
+        setErrorMessage(err.message || "oops, something went wrong!");
+      }
+    },[]);
+      
+    return {
+        loading,
+        errorMessage,
+        sendRequest
+    }
+}
+
+export default useHttp;
