f741692eea86d4d2a0b16bc3830c51580791e928
s.feli <s.feli@DESKTOP-LOCAL>
2022-07-13 11:01:41 +0430

    go to searched user page when search form submits

--- src/components/Home/SearchForm.tsx
+++ src/components/Home/SearchForm.tsx
@@ -1,21 +1,21 @@
+import { useHistory } from 'react-router-dom';
 import React, { useRef, useState } from 'react';
 import classes from '../../styles/SearchForm.module.scss';
 
 const SearchForm = () => {
     const [validationMessage,setValidationMessage] = useState<string>("");
-
+    const history = useHistory();
     const userNameRef = useRef<HTMLInputElement>(null);
 
-    const submitHandler = (e: React.FormEvent) => {
+    const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const userName = userNameRef.current!.value;
         if (userName.trim().length < 3){
             setValidationMessage("Please enter at least 3 character");
         }else{
-            debugger;
+            history.push(userName.trim());
         }
     }
-    
     const changeInputHandler = (e: React.FormEvent<HTMLInputElement>) => {
         const enteredValue = e.currentTarget.value;
         if (enteredValue.trim().length >= 3){
@@ -25,7 +25,6 @@ const SearchForm = () => {
 
     return (
         <form className={classes.form} onSubmit={submitHandler}>
-            <h2>Search User</h2>
             <div className={classes.searchGroup}>
                 <input 
                     type="text" 
