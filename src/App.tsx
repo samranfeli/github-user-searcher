cb34d932d3642473df5342455f0218c1fb6be86a
s.feli <s.feli@DESKTOP-LOCAL>
2022-07-13 10:58:24 +0430

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import {Route,Switch} from 'react-router-dom';
+
 import Home from "./pages/Home";
+import User from './pages/User';
 import './styles/global.scss';
 
 function App() {
   return (
-    <div className="container">
-      <Home />
+    <div>
+      <Switch>
+        <Route path={'/:username'}>
+          <User />
+        </Route>
+        <Route path={'/'}>
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
